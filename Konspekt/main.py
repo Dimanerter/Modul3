69f91545e008a2fe50fb7245a1df81ca7aa76635
Dimanerter <erterujamp@gmail.com>
2024-09-18 18:13:29 +0200

    napoleon_burns_moscow

--- Konspekt/main.py
+++ Konspekt/main.py
@@ -1,8 +1,12 @@
-from datetime import datetime 
+from datetime import datetime
 
-seventh_day_2019 = datetime(year = 2019, month=1, day=7, hour=14)
-seventh_day_2020 = datetime(year = 2020, month=1, day=7, hour=14)
+napoleon_burns_moscow = datetime(year=1812, month=9, day=14)
 
-difference = seventh_day_2020 - seventh_day_2019
-print(difference)
-print(difference.total_seconds())
+current_date = datetime.now()
+
+days_since = current_date.toordinal() - napoleon_burns_moscow.toordinal()
+
+print(days_since)
+
+
+
