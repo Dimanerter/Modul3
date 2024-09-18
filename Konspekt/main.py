6afa8b2f6ec8c81754b5def343199cd4e238c225
Dimanerter <erterujamp@gmail.com>
2024-09-18 16:20:07 +0200

    combined_datetime

--- Konspekt/main.py
+++ Konspekt/main.py
@@ -1,4 +1,8 @@
 import datetime
 
-now = datetime.datetime.now()
-print(now)
+date_part = datetime.date(2023, 12,14)
+time_part = datetime.time(12,30,15)
+
+combined_datetime = datetime.datetime.combine(date_part, time_part)
+
+print(combined_datetime)
