0a3af34661c6d3feb47a909ca09c62116f8634c1
Dimanerter <erterujamp@gmail.com>
2024-09-19 12:48:21 +0200

    isoformat

--- Konspekt/main.py
+++ Konspekt/main.py
@@ -1,7 +1,13 @@
-from datetime import datetime
+from datetime import datetime 
 
-date_string = "2023.03.14"
+now = datetime.now()
 
-datetime_object = datetime.strptime(date_string, "%Y.%m.%d")
-print(datetime_object)
+# Конвертация в формат ISO 8601
+iso_format = now.isoformat()
+print(iso_format)
 
+iso_date_string = "2023-03-14T12:39:29.992996"
+
+# Конвертация из ISO формата
+date_from_iso = datetime.fromisoformat(iso_date_string)
+print(date_from_iso)
