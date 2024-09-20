541b333696b345d73c906028c770b0ec04119d3a
Dimanerter <erterujamp@gmail.com>
2024-09-20 13:04:06 +0200

    iso_format_with_timezone

--- Konspekt/main.py
+++ Konspekt/main.py
@@ -1,6 +1,9 @@
 from datetime import datetime, timezone, timedelta
 
-utc_time = datetime.now(timezone.utc)
+# Час у конкретній часовій зоні
+timezone_offset = timezone(timedelta(hours=2))  # Наприклад, UTC+2
+timezone_datetime = datetime(year=2023, month=3, day=14, hour=12, minute=30, second=0, tzinfo=timezone_offset)
 
-eastern_time = utc_time.astimezone(timezone(timedelta(hours=-5)))
-print(eastern_time)
+# Конвертація у формат ISO 8601
+iso_format_with_timezone = timezone_datetime.isoformat()
+print(iso_format_with_timezone)
