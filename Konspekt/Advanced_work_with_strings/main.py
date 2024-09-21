28eea2116d0352624401e50cf8f29b0abc1c989a
Dimanerter <erterujamp@gmail.com>
2024-09-21 17:29:12 +0200

    Advanced work with strings

---
+++ Konspekt/Advanced_work_with_strings/main.py
@@ -0,0 +1,16 @@
+text = """This is first line 
+And second line
+Last third line"""
+
+print(text)
+
+
+print("Hello\nWorld")
+print("Hello\tWorld")
+print("Hello my little\rsister")
+print("Hello\bWorld")
+print("Hello\\World")
+print("Hello\n\vWorld")
+print("it\'s a beautiful day")
+print("He said, \"Hello\"")
+
