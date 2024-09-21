ebc0f78e4c882b9c99840706458989d5c1d321c8
Dimanerter <erterujamp@gmail.com>
2024-09-21 18:17:56 +0200

    метод find

--- Konspekt/Advanced_work_with_strings/main.py
+++ Konspekt/Advanced_work_with_strings/main.py
@@ -14,3 +14,16 @@ print("Hello\n\vWorld")
 print("it\'s a beautiful day")
 print("He said, \"Hello\"")
 
+
+# метод find
+
+s = "Hi there!"
+
+start = 0
+end = 7
+
+print(s.find("er", start, end))
+print(s.find("q"))
+
+
+
