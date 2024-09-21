e2d7c3568940b661183a951848aeba73841d703b
Dimanerter <erterujamp@gmail.com>
2024-09-21 17:00:04 +0200

    math_end

--- Konspekt/math/main.py
+++ Konspekt/math/main.py
@@ -14,4 +14,29 @@ trunc_result = math.trunc(x)
 
 print(ceil_result, floor_result, trunc_result)
 
+# Приклад використання пакета math
+# Використання констант
+print(math.pi)
+
+# Тригонометрія
+angle = math.radians(60)
+print(math.sin(angle))
+
+# Корінь числа
+print(math.sqrt(9))
+
+# Логарифми
+print(math.log(10, 2))
+
+
+# Сравнение
+
+print(0.1 + 0.2 == 0.3)
+# Сравнение math 
+
+r = math.isclose(0.1 + 0.2, 0.3)
+print(r)
+
+
+
 
