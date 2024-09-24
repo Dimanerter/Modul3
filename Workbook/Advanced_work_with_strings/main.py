f1c76793de3f6b88f2023604ec20a21e58d4ae11
Dimanerter <erterujamp@gmail.com>
2024-09-24 17:29:02 +0200

    Text to Morze

--- Workbook/Advanced_work_with_strings/main.py
+++ Workbook/Advanced_work_with_strings/main.py
@@ -81,3 +81,56 @@ for el in query.split('&'):
     key, value = el.split('=')
     obj_query.update({key: value.replace('+', ' ')})
 print(obj_query)
+
+
+# Translate
+
+intab = "aeiou"
+outtab = "12345"
+trantab = str.maketrans(intab, outtab)
+
+stri = "This is string example"
+print(stri.translate(trantab))
+
+trantab = str.maketrans('', '', intab)
+stri = "This is string example"
+print(stri.translate(trantab))
+
+# from 16 to 2
+
+symbols = "0123456789ABCDEF"
+code = [
+        '0000', '0001', '0010', '0011', '0100', '0101', '0110', '0111',
+        '1000', '1001', '1010', '1011', '1100', '1101', '1110', '1111'
+       ]
+MAP = {}
+
+for s, c in zip(symbols, code):
+    MAP[ord(s)] = c
+    MAP[ord(s.lower())] = c
+
+result = "34 DF 56 AC".translate(MAP)
+print(result)
+
+
+# Text to Morze
+
+morze_dict = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+              'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+              'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+              'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+              'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---',
+              '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+              '8': '---..', '9': '----.'}
+table_morze_dict = {}
+for k, v in morze_dict.items():
+    table_morze_dict[ord(k)] = v
+
+string = "Hello world"
+
+result_ = ""
+
+for ch in string:
+    result_ = result_ + ch.upper().translate(table_morze_dict)
+
+print(result_)
